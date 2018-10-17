3b34995f1ac84b4a7f6743b95fe8d0c2151f0997
wangjunchao <2931748796@qq.com>
2018-10-17 12:35:14 +0800

    物流追踪号码可拨打

--- app/src/main/java/e/hanglungdemo/view/adapter/TraceListAdapter.java
+++ app/src/main/java/e/hanglungdemo/view/adapter/TraceListAdapter.java
@@ -16,14 +16,22 @@ import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.text.style.TextAppearanceSpan;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.entity.LocalMedia;
 
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -36,6 +44,8 @@ import e.hanglungdemo.MainActivity;
 import e.hanglungdemo.R;
 import e.hanglungdemo.utils.SpannableStringUtils;
 import e.hanglungdemo.view.bean.TraceBean;
+import e.hanglungdemo.view.home.activity.SignInActivity;
+import e.library.BaseDialog;
 import e.library.T;
 
 public class TraceListAdapter extends BaseAdapter {
@@ -44,6 +54,7 @@ public class TraceListAdapter extends BaseAdapter {
     private static final int TYPE_TOP = 0x0000;
     private static final int TYPE_NORMAL = 0x0001;
     private Intent intent;
+    private BaseDialog dialog;
 
     public TraceListAdapter(Context context, List<TraceBean> traceBeanList) {
         this.context = context;
@@ -106,18 +117,11 @@ public class TraceListAdapter extends BaseAdapter {
             holder.ivGoodsTwo.setVisibility(View.GONE);
             holder.tvDot.setBackgroundResource(R.mipmap.timelline_dot_first);
         }
-
         holder.tvAcceptTime.setText(traceBean.getAcceptTime());
-        holder.tvAcceptStation.setText(traceBean.getAcceptStation());
         SpannableStringUtils sp = new SpannableStringUtils(traceBean.getAcceptStation());
-
         checkPhoneText(holder.tvAcceptStation, sp, traceBean.getAcceptStation());
         return convertView;
     }
-
-
-
-
     @Override
     public int getItemViewType(int id) {
         if (id == 0) {
@@ -137,17 +141,19 @@ public class TraceListAdapter extends BaseAdapter {
         Pattern pattern = Pattern
                 .compile("\\d{3}-\\d{8}|\\d{4}-\\d{7}|\\d{11}");
         final Matcher matcher = pattern.matcher(text);
-
         int start = 0;
         //遍历取出字符串中所有的符合条件的；
         while (matcher.find(start)) {
             start = matcher.end();
             sp.setColor(Color.BLUE, matcher.start(), matcher.end())
+                    .setBackGround(ContextCompat.getColor(context,R.color.white), matcher.start(), matcher.end())
+                    .setBold( matcher.start(), matcher.end())
+                    .setOnClick(matcher.start(), matcher.end(), ContextCompat.getColor(context,R.color.blue))
+
                     .setOnClickSpanListener(new SpannableStringUtils.OnClickSpanListener() {
                         @Override
                         public void OnClickSpanListener() {
-
-
+                            showDialog(Gravity.BOTTOM, R.style.Bottom_Top_aniamtion,matcher.group());
                         }
                     });
 
@@ -161,5 +167,41 @@ public class TraceListAdapter extends BaseAdapter {
         tvAcceptStation.setText(sp);
 
     }
+
+
+    private void showDialog(int grary, int animationStyle, final String group) {
+        BaseDialog.Builder builder = new BaseDialog.Builder(context);
+        dialog = builder.setViewId(R.layout.phone_dialog)
+                .setPaddingdp(10, 0, 10, 0)
+                .setGravity(grary)
+                .setAnimation(animationStyle)
+                .setWidthHeightpx(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                .isOnTouchCanceled(true)
+                .addViewOnClickListener(R.id.bt_callPhone, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        intent = new Intent(Intent.ACTION_CALL);
+                        Uri data = Uri.parse("tel:" + group);
+                        intent.setData(data);
+                        context.startActivity(intent);
+                        dialog.close();
+                    }
+                })
+                .addViewOnClickListener(R.id.bt_dissmess, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        dialog.close();
+                    }
+                })
+                .builder();
+        dialog.show();
+        Button phone= dialog.getView(R.id.bt_callPhone);
+        phone.setText(group);
+
+    }
+
+
+
 
 }
